92af423a759b7bf2ca9e8d796265eb2607fdea06
Ivan Yakushenko <kshnkvn@gmail.com>
2021-02-16 14:11:29 +0200

    output file path in config

--- config.py
+++ config.py
@@ -7,6 +7,10 @@ class Config:
 
     BASE_DIR = path.abspath(path.dirname(__file__))
 
+    # full path to output JSON-file
+    OUTPUT_FILE_PATH = path.abspath(
+        getenv('OUTPUT_FILE_PATH', path.join(BASE_DIR, 'output.json')))
+
     # full path to json-file with array of urls
     # which need to scrape
     DOWNLOAD_LIST_PATH = getenv('DOWNLOAD_LIST_PATH', None)
