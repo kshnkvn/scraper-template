84b781cd5ca1a7bd6c0e86e7133ca817b6d98f85
Ivan Yakushenko <kshnkvn@gmail.com>
2021-02-16 13:31:33 +0200

    init and start downloader and parser functions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from multiprocessing import Process
 
 from config import Config
 from scraper import Downloader, Parser, Writer
@@ -12,3 +13,24 @@ logging.basicConfig(
     level=logging.INFO, 
     format='[%(asctime)s] [%(levelname)s]: %(message)s'
 )
+
+# determine the source from which scraping will be
+# only one source will be used. priority LIST > URL
+source = None
+if bool(Config.DOWNLOAD_LIST):
+    source = Config.DOWNLOAD_LIST
+elif Config.START_URL:
+    source = Config.START_URL
+else:
+    logging.error(f'Source not specified for the scraper')
+    sys.exit(-1)
+
+downloader = Downloader(source, Config.REDIS_URI, Config.DOWNLOAD_LIMIT)
+parser = Parser(Config.REDIS_URI, Config.PARSE_LIMIT)
+
+downloader_process = Process(target=downloader.run)
+downloader_process.start()
+
+parser_process = Process(target=parser.run)
+parser_process.start()
+parser_process.join()
